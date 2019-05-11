4b0c994785bb09bd6163442e5d77ce1034ed7e7a
danila19991 <guseffdanila@ya.ru>
2019-05-11 23:04:31 +0300

    add tcp server

--- RobinBot_backend/app/apps.py
+++ RobinBot_backend/app/apps.py
@@ -1,5 +1,36 @@
 from django.apps import AppConfig
 
+import socket
+import threading
+from . import views
+import os
+
+
+def tcp_server(ip, port):
+    print('start tcp server')
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((ip, port))
+    s.listen(1)
+    while True:
+        conn, addr = s.accept()
+
+        print('connected:', addr)
+
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            views.process_message(data.decode())
+
+        conn.close()
+
 
 class AppConfig(AppConfig):
     name = 'app'
+
+    def ready(self):
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            t = threading.Thread(target=tcp_server, args=('localhost', 9090))
+            t.daemon = True
+            t.start()
